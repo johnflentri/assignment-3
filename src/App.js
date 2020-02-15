507de7fc3eb46daf1804d4f85fc212f1c75e6e3d
johnflentri <jpflentri@gmail.com>
2020-02-15 12:44:49 +0100

    Section 2 components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import QuoteSearcher from './components/QuoteSearcher';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <QuoteSearcher />
+    </div>
   );
 }
 
