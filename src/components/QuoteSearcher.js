4f202d670e3a7f94e1ddc1ee9a5da9f19548fc3f
johnflentri <jpflentri@gmail.com>
2020-02-15 14:29:03 +0100

    Add componentDidMount and loading property while fetching data

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,27 +3,32 @@ import Quotes from "./Quotes"
 
 export default class QuoteSearcher extends Component {
   state = {
-    quotes: [
-      {
-        "_id": "5d91b45d9980192a317c8acc",
-        "quoteText": "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
-        "quoteAuthor": "Bruce Lee"
-      },
-      {
-        "_id": "5d91b45d9980192a317c8abe",
-        "quoteText": "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
-        "quoteAuthor": "Abraham Lincoln"
-      },
-      {
-        "_id": "5d91b45d9980192a317c8955",
-        "quoteText": "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
-        "quoteAuthor": "J. Willard Marriott"
-      },
-    ]
+    fetching: true,
+    quotes: []
   }
+
+  componentDidMount() {
+    fetch('https://quote-garden.herokuapp.com/quotes/search/tree')
+      .then(res => res.json())
+      .then(quoteSearch => {
+        // console.log(quoteSearch.results)
+        this.setState({
+          fetching: false,
+          quotes: quoteSearch.results
+        })
+      })
+      .catch(console.error)
+  }
+
   render() {
-    return (
-      <Quotes text={this.state.quotes.map(quoteData => <p>{quoteData.quoteText + " By: " + quoteData.quoteAuthor}</p>)} />
-    )
+    if (this.state.fetching) {
+      return <div>"Loading..."</div>
+    } else if (this.state.error) {
+      return <div>"Error"</div>
+    } else {
+      return (
+        <Quotes text={this.state.quotes.map(quoteData => <p>{quoteData.quoteText + " By: " + quoteData.quoteAuthor}</p>)} />
+      )
+    }
   }
 }
